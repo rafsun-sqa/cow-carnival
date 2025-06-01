be9ad57540f9da589d9e0dabb2e71e64ca1d6d83
rafsun-sqa <rafsun.sqa@gmail.com>
2025-06-01 17:49:50 +0600

    Update multiple pages with new content

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function generateCowName() {
-  const names = ["Moozilla", "Bhuri Bhai", "Lal Badshah", "Moti", "Kalu"];
+  const names = ["Moozilla", "Bhuri Bhai", "Lal Badshah", "Moti", "Kalu","Habba King,"Shamsher king","Golu","Bholu","Mastani","Rani","Chutki","Babu","Chintu","Pinky","Ladoo","Golu Bhai"];
   const name = names[Math.floor(Math.random() * names.length)];
   document.getElementById("cow-name-result").innerText = `Your cow name is: ${name}`;
 }
